9c1479f041aef8ac67c7524fe97872fd39e1682d
sawermager <wermager3004@gmail.com>
2020-05-31 19:50:46 -0500

    added protocols for container, size, and iter

--- impl_collections/sorted_set.py
+++ impl_collections/sorted_set.py
@@ -1,4 +1,13 @@
 class SortedSet:
     
-    def __init__(self, items):
-        self._items = sorted(items)
+    def __init__(self, items=None):
+        self._items = sorted(set(items)) if items is not None else []
+
+    def __contains__(self, item):
+        return item in self._items
+
+    def __len__(self):
+        return len(self._items)
+
+    def __iter__(self):
+        return iter(self._items)
--- impl_collections/test_sorted_set.py
+++ impl_collections/test_sorted_set.py
@@ -26,10 +26,10 @@ class TestConstruction(unittest.TestCase):
         Check that we can construct an instance from an
         arbituary iterable.
         Note: In this case, I've used a generator (since generators
-        return iterables). The goal here is to show that we can 
-        successfully construct a sorted set in a way that is 
-        typical for python collections. We want to know that we
-        can construct a sorted collection from iterable 
+        return generators, which are iterators). The goal here is to 
+        show that we can successfully construct a sorted set in a way 
+        that is typical for python collections. We want to know that 
+        we can construct a sorted collection from iterable 
         sequences of various configurations, including being empty.
         We don't want to rely on the argument being passed to the 
         constructor being anything more sophisticated than an 
@@ -43,6 +43,62 @@ class TestConstruction(unittest.TestCase):
             yield 2
         g = gen6842()
         s = SortedSet(g)
+
+    def test_default_empty(self):
+        s = SortedSet()
+    
+class TestContainerProtocol(unittest.TestCase):
+    
+    def setUp(self):
+        self.s = SortedSet([6,7,3,9])
+
+    def test_positive_contained(self):
+        self.assertTrue(6 in self.s)
+    def test_negative_contained(self):
+        self.assertFalse(2 in self.s)
+    def test_positive_not_contained(self):
+        self.assertTrue(5 not in self.s)
+    def test_negative_not_contained(self):
+        self.assertFalse(9 not in self.s)
+
+class TestSizeProtocol(unittest.TestCase):
+
+    def test_empty(self):
+        s = SortedSet()
+        self.assertEqual(len(s), 0)
+        
+    def test_one(self):
+        s = SortedSet([42])
+        self.assertEqual(len(s), 1)
+    
+    def test_ten(self):
+        s = SortedSet(range(10))
+        self.assertEqual(len(s), 10)
+        
+    def test_with_duplicates(self):
+        s = SortedSet([1,1,1])
+        self.assertEqual(len(s), 1)
+
+class TestIterableProtocol(unittest.TestCase):
+    
+    def setUp(self):
+        self.s = SortedSet([7,2,1,1,9])
+
+    def test_iter(self):
+        i = iter(self.s)
+        self.assertEqual(next(i), 1)
+        self.assertEqual(next(i), 2)
+        self.assertEqual(next(i), 7)
+        self.assertEqual(next(i), 9)
+        self.assertRaises(StopIteration,lambda: next(i))
+    
+    def test_for_loop(self):
+        index = 0
+        expected = [1,2,7,9]
+        for item in self.s:
+            self.assertEqual(item, expected[index])
+            index += 1
+        
         
 if __name__ == "__main__":
     unittest.main()
